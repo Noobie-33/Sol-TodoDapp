fefe3c64a11cf103996bdfb210755ea3d630ef3a
Noobie-33 <noobprogrammer95@gmail.com>
2025-04-29 12:06:27 +0300

    next.confign changed

--- app/next.config.js
+++ app/next.config.js
@@ -1,17 +1,38 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: false,
-  webpack(config) {
+  webpack5: true,
+  webpack: (config) => {
+    // Add fallbacks for certain Node modules
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
       os: false,
-    };
-    return config;
+    }
+
+    // Configure .mjs files to be treated as JavaScript
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    })
+
+    // Custom alias for the Solana wallet adapter package
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@solana/wallet-adapter-react': path.resolve(
+        './node_modules/@solana/wallet-adapter-react'
+      ),
+    }
+
+    return config
   },
+
+  // Set up environment variables
   env: {
     NEXT_PUBLIC_RPC_HOST: 'https://metaplex.devnet.rpcpool.com/',
+    // NEXT_PUBLIC_RPC_HOST: 'https://api.metaplex.solana.com/',
   },
-};
-
-module.exports = nextConfig;
+}
